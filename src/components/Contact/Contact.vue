b3c083801dc61bec0f22f36cfb013ee32380bbe7
Kelsey Hawley <khawley@smarttech.com>
2018-05-17 00:58:46 -0700

    touch-up contact

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -7,19 +7,20 @@
          frameborder="0"
          style="border:0"
          allowfullscreen).map
-  .contact-info
+  .contact-info-container
     .phone-local.contact-container.phone
       img(src="../../assets/icon-phone.svg" href="tel:+1-604-599-4200").icon.icon-24.icon-phone
-      a.phone.contact-info(href="tel:+1-604-599-4200") (604) 599-4200
-    .phone-itl.contact-container.phone
-      img(src="../../assets/icon-phone.svg" href="tel:+1-877-699-4200").icon.icon-24.icon-phone
-      a.phone.contact-info(href="tel:+1-877-699-4200") +1 (877) 669-4200
+      .phone.contact-info.header Give us a Ring!
+      a.phone.contact-info.newline(href="tel:+1-604-599-4200") (604) 599-4200
+      a.phone.contact-info.newline(href="tel:+1-877-699-4200") +1 (877) 669-4200
     .email.contact-container
       img(src="../../assets/icon-email.svg").icon.icon-24.icon-email
-      a.email.contact-info(href="mailto:info@megacranes.com") info@megacranes.com
+      .email.contact-info.header Email Us
+      a.email.contact-info.newline(href="mailto:info@megacranes.com") info@megacranes.com
     .address.contact-container
       img(src="../../assets/icon-directions.svg").icon.icon-24.icon-email
-      .address.contact-info 6330 148 St.
+      .address.contact-info.header Directions
+      .address.contact-info.newline 6330 148 St.
       .address.contact-info.newline Surrey, B.C.
       .address.contact-info.newline V3X 3C4
   .clear
@@ -63,43 +64,58 @@ export default {
 
   .map {
     width: 45vw;
-    float: left;
     position: relative;
     display: inline-block;
     padding: 0 25px;
   }
 
-  .contact-info {
+  .contact-info-container {
     width: 40vw;
-    padding: 0 25px;
+    // padding: 0 25px;
     display: inline-block;
     color: $white;
+    height: 100%;
+    vertical-align: top;
 
     .contact-container {
-      position: relative;
+      width: 300px;
+      margin: 20px;
       display: block;
-      padding: 20px;
+      overflow-y: auto;
+      overflow-x: hidden;
+      vertical-align: middle;
 
       .icon {
-        // padding-right: 10px;
+        padding: 2px;
+        display: inline-block;
       }
 
       .contact-info {
-        position: absolute;
-        line-height: 24px;
+        // position: absolute;
+        display: inline-block;
+        padding: 5px 20px;
+        text-align: center;
+
+        &.header {
+          vertical-align: top;
+          font-weight: 900;
+          line-height: 20px;
+        }
 
         &.newline {
+          display: block;
           position: relative;
-          float: left;
           margin-left: 25px;
-          margin-bottom: 4px;
+          line-height: 18px;
+          color: $white;
+
+          &:hover {
+            color: $mega-blue;
+          }
         }
       }
     }
   }
 
-  .clear {
-    clear: both;
-  }
 }
 </style>
